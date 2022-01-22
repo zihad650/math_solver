3d0a20784d5487114845f59d9a5db0eaa8a6931b
nurunnobe <64194094+nurunnobejisan@users.noreply.github.com>
2022-01-23 02:34:02 +0600

    jisan

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:math_solver/Design%20page/design_2nd_page.dart';
 
@@ -6,6 +8,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +16,48 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo,
       ),
-      home: Design2ndPage(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
+
+  }
+
+}
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("images/font_page.png"), fit: BoxFit.cover),
+      ),);
+  }
+
+  @override
+  void initState() {
+    Timer(Duration(seconds: 6), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => Design2ndPage()));
+    });
   }
 }
-//thank you
+
